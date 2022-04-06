10fba1404e310a8b443064c8f3a49d4cca83b850
Kahliaha <kahliahahen@gmail.com>
2022-04-06 07:34:11 -0400

    edited txt name to election results

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -8,7 +8,7 @@ import os
 # Add a variable to load a file from a path.
 file_to_load = os.path.join("Resources", "election_results.csv")
 # Add a variable to save the file to a path.
-file_to_save = os.path.join("analysis", "election_analysis.txt")
+file_to_save = os.path.join("analysis", "election_results.txt")
 
 # Initialize a total vote counter.
 total_votes = 0
@@ -117,6 +117,7 @@ with open(file_to_save, "w") as txt_file:
 
     # 7: Print the county with the largest turnout to the terminal.
     winning_county_summary=(
+        f"--------------------------\n"
         f"Largest County Turnout: {winning_county}\n"
         f"--------------------------\n")
     print(winning_county_summary)
